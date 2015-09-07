1ea24dc08cb2e9681330a7002839a81ee13327aa
maykristine <maykristine@users.noreply.github.com>
2015-09-07 17:16:16 +0900

    Create Rock-paper-scissors-lizard-Spock.py

---
+++ Rock-paper-scissors-lizard-Spock.py
@@ -0,0 +1,103 @@
+import random
+# Rock-paper-scissors-lizard-Spock
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+def name_to_number(name):
+    ## alternative:
+    # names = ["rock", "Spock", "paper", "lizard", "scissors"]
+    ## for error handling
+    # if name not in names:
+    #	return 5
+    # else:
+    #	return names.index(name)
+
+    # convert name to number using if/elif/else
+    if name == "rock":
+        number = 0
+    elif name == "Spock":
+        number = 1
+    elif name == "paper":
+        number = 2
+    elif name == "lizard":
+        number = 3
+    elif name == "scissors":
+        number = 4
+    else:
+        number = 5
+        # for error handling
+        
+    # don't forget to return the result!
+    return number
+
+def number_to_name(number):
+    ## alternative:
+    # names = ["rock", "Spock", "paper", "lizard", "scissors"]
+    # return names(number)
+    
+    # convert number to a name using if/elif/else
+    if number == 0:
+        name = "rock"
+    elif number == 1:
+        name = "Spock"
+    elif number == 2:
+        name = "paper"
+    elif number == 3:
+        name = "lizard"
+    else:
+        name = "scissors"
+        
+    # don't forget to return the result!
+    return name
+
+def rpsls(player_choice): 
+    # print a blank line to separate consecutive games
+    print "\n"
+
+    # print out the message for the player's choice
+    print "Player chooses %s" % player_choice
+
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+    # error-handling in anticipation of user input for player_choice
+    if player_number == 5:
+        print "Invalid player input"
+        return
+
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0,5)
+
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+    
+    # print out the message for computer's choice
+    print "Computer chooses %s" % comp_choice
+
+    # compute difference of comp_number and player_number modulo five
+    result = (comp_number - player_number) % 5
+
+    # use if/elif/else to determine winner, print winner message
+    if result == 1 or result == 2:
+        print "Computer wins!"
+    elif result == 3 or result == 4:
+        print "Player wins!"
+    else:
+        print "Player and computer tie!"
+
+    
+# test code
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
